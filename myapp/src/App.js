9b31e56136b1b049e5a6db505864bd2ef31a2851
i_am_akash_ <ay157311@gmail.com>
2020-12-14 19:02:42 +0530

    porps

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello Akash</h1>
-        {/* Nesting Component */}
-        <Ninjas />  
+        <h1>Hello Akash</h1>  
+        {/* Sendig props to Ninjas Component*/}
+        <Ninjas name="Akash" age="22" belt="black"/> 
+        <Ninjas name="Anubhav" age="8" belt="yellow"/>  
       </div>
     );
   }
