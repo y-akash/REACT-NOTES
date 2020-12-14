788438caa8543c4ed52c1b12f5e974244e63a02f
i_am_akash_ <ay157311@gmail.com>
2020-12-14 13:56:04 +0530

    first ract app

---
+++ myapp/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello Akash</h1>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Hello World</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
