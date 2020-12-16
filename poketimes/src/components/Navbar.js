9b3fc96bc8219fb68df930882620883e05889d11
i_am_akash_ <ay157311@gmail.com>
2020-12-16 08:03:57 +0530

    Link and NavLink

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 // if we use <a></a> tag than page reload we dont want that therfore we use Link or NavLink
-
+// Link or NavLink dont reload or sends the request to server
+// Link and NavLink both work same and 
+// react automatically give anchor tag on html page it will not show Link and NavLink tag on the HTML page
+// but NavLink add class="active" in anchor tag remaing all are same
 
 const Navbar = () => {
   return (
@@ -9,9 +13,9 @@ const Navbar = () => {
       <div className="container">
         <a className="brand-logo">Poke' Times</a>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
         </ul>
       </div>
     </nav> 
