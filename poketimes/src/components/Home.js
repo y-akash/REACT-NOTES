a1ef3aee91d36684294e9419b10faeba0949cb1d
i_am_akash_ <ay157311@gmail.com>
2020-12-16 17:31:35 +0530

    images

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';   // axios is a http request library     install from npm
 import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 
 class Home extends Component {
     state = {
@@ -22,14 +23,15 @@ class Home extends Component {
         const postList = posts.length ? (
         posts.map(post => {
             return (
-            <div className="post card" key={post.id}>
-                <div className="card-content">
-                <Link to={'/' + post.id}>
-                    <span className="card-title red-text">{post.title}</span>
-                </Link>
-                <p>{post.body}</p>
+                <div className="post card" key={post.id}>
+                    <img src={Pokeball} alt="A Pokeball" />
+                    <div className="card-content">
+                    <Link to={'/' + post.id}>
+                        <span className="card-title red-text">{post.title}</span>
+                    </Link>
+                    <p>{post.body}</p>
+                    </div>
                 </div>
-            </div>
             )
         })
         ) : (
