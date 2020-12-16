2b8e1cd9a44d2cb1f1ef371baaebc2ab6cb1698b
i_am_akash_ <ay157311@gmail.com>
2020-12-16 15:56:04 +0530

    used  axios
    Here we are fetching a data from url using axios which is a http request library we can also use fetch

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';   // axios is a http request library     install from npm
 
-const Home = ()=>{
-    return(
-        <div className="container">
+class Home extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        // axios is a http request library
+        // we can also use fetch instead of axios.
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(res => {
+            console.log(res);
+            this.setState({
+            posts: res.data.slice(0,10)
+            });
+        })
+    }
+    render(){
+        const { posts } = this.state
+        const postList = posts.length ? (
+        posts.map(post => {
+            return (
+            <div className="post card" key={post.id}>
+                <div className="card-content">
+                <span className="card-title">{post.title}</span>
+                <p>{post.body}</p>
+                </div>
+            </div>
+            )
+        })
+        ) : (
+        <div className="center">No posts to show</div>
+        );
+
+        return (
+        <div>
+            <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto possimus provident corrupti sed ad saepe reprehenderit atque nemo hic? Corrupti vero labore qui ad iure at quidem deserunt beatae nesciunt </p>
+            {postList}
+            </div>
         </div>
-    );
+        )
+    }
 }
 
 export default Home;
