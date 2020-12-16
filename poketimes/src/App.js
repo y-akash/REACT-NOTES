986c838be330449a963dff55d366c3aa7080d6e6
i_am_akash_ <ay157311@gmail.com>
2020-12-16 17:15:33 +0530

    react parameter

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post'
 
 class App extends Component {
   render() {
@@ -14,6 +15,11 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
+          {/* when we try to go contact or about  url it will show contact or about component */}
+          {/* and after that also shows post component */}
+          {/* bcoz it is considering  /something so /contact and /post_id matches with them. */}
+          {/* so to avoid this we will use switch in 32-switch branch */}
+          <Route path="/:post_id" component={Post}/>
         </div>
       </BrowserRouter>
     );
