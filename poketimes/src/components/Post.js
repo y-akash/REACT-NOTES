008e688af5dbe520843e712532bf568ebb5a8267
i_am_akash_ <ay157311@gmail.com>
2020-12-18 19:30:23 +0530

    action folder created
    new action folder created so that all the action we can separate out it is just to maintain the code easily

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
 
@@ -51,7 +52,7 @@ const mapStateToProps = (state, ownProps) =>{
 const mapDispatchToProps = (dispatch) => {
   // it will return the object to props
   return {
-    deletePost: (id) => dispatch({type: 'DELETE_POST', id: id})
+    deletePost: (id) => dispatch(deletePost(id))
   }
 }
 
