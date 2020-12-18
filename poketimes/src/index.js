2f8147daff94c9d0bb2b044b6a1baf3b0da104a6
i_am_akash_ <ay157311@gmail.com>
2020-12-18 11:28:03 +0530

    setting redux in react

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer);
+
+// Provider is a component which wrapped the root <App> Component
+// and we pass the store in provider which provides a store to our application
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
